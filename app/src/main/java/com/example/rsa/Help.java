3e7a2d40ef8c1c3865a7d30423ff95fe3f72634c
NasinaNirhy <128202616+NasinaNirhy@users.noreply.github.com>
2023-03-18 06:25:36 +0300

    Update Help.java

--- app/src/main/java/com/example/rsa/Help.java
+++ app/src/main/java/com/example/rsa/Help.java
@@ -1,1 +1,14 @@
+package com.example.rsa;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Help extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
